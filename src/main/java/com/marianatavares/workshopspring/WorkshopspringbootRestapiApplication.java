35a92fadf336cfe16f6048e946b4325f38b74ea3
mariana tavares <marianasr.tavares@gmail.com>
2022-04-23 14:02:45 +0100

    Projeto criado

---
+++ src/main/java/com/marianatavares/workshopspring/WorkshopspringbootRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.marianatavares.workshopspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopspringbootRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopspringbootRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marianatavares/workshopspring/WorkshopspringbootRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marianatavares.workshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringbootRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
