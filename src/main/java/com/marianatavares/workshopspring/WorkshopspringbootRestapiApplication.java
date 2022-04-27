ef1bd8cf6146cfd8922af59df44613cad9ee55bb
Mariana Tavares <marianasr.tavares@gmail.com>
2022-04-27 21:44:05 +0100

    Posts com comentarios

--- src/main/java/com/marianatavares/workshopspring/WorkshopspringbootRestapiApplication.java
+++ src/main/java/com/marianatavares/workshopspring/WorkshopspringbootRestapiApplication.java
@@ -8,6 +8,73 @@ public class WorkshopspringbootRestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopspringbootRestapiApplication.class, args);
+//		String[] test= {"co","de","ar","s"};
+//		System.out.println(name(test));
+//		
+//		
+//		
+//	}
+//	public static int name(String[] s) {
+//		int total=0;
+//		int max=0;
+//		int size=0;
+//		
+//		
+//			String name="";
+//			name=s[0];
+//			int j=0;
+//			int z=0;
+//			while(z<s.length) {
+//				if(rule(totalString(s))) {
+//					max=value(totalString(s));
+//					size= totalString(s).length();
+//					break;
+//				}
+//			z++;
+//			j=0;
+//			while(j<s.length) {
+//				if(j!=0 && j!=z) {
+//				name+=s[j];
+//				}
+//				j++;
+//		}
+//
+//			if(rule(name)) {
+//			total=value(name);
+//			size=name.length();
+//			if(total>max) {
+//				max=total;
+//			}
+//			System.out.println(name);
+//			name="";
+//			}
+//			}
+//			
+//		
+//		
+//		return size;
+//	}
+//	
+//	public static int value(String s) {
+//		int sum=0;
+//		for(int i=0;i<s.length();i++) {
+//			sum+=s.charAt(i)-96;
+//		}
+//		return sum;
+//	}
+//	public static boolean rule(String s) {
+//		return value(s)<= 10*s.length();
+//	}
+//	
+//	public static String totalString(String[] s) {
+//		String sum="";
+//	
+//		for(int i=0;i<s.length;i++) {
+//			sum+=s[i];
+//	}
+//		return sum;
+//	
+//	}
 	}
 
 }
--- src/main/java/com/marianatavares/workshopspring/config/Instantiation.java
+++ src/main/java/com/marianatavares/workshopspring/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.marianatavares.workshopspring.config.dto.AuthorDTO;
+import com.marianatavares.workshopspring.config.dto.CommentDTO;
 import com.marianatavares.workshopspring.domain.Post;
 import com.marianatavares.workshopspring.domain.User;
 import com.marianatavares.workshopspring.repository.PostRepository;
@@ -38,11 +39,19 @@ public class Instantiation implements CommandLineRunner {
 	    Post post1 = new Post (null, sdf.parse("2018-03-21"), "Partiu viagem","Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 	    Post post2 = new Post (null, sdf.parse("2018-03-23"), "Bom dia","Acordei feliz hoje!",new AuthorDTO(maria));
 
+	    CommentDTO comment1 = new CommentDTO("Boa viagem mano!",sdf.parse("2018-03-21"), new AuthorDTO(alex));
+	    CommentDTO comment2 = new CommentDTO("Aproveite!",sdf.parse("2018-03-22"), new AuthorDTO(bob));
+	    CommentDTO comment3 = new CommentDTO("Boa viagem mano!",sdf.parse("2018-03-21"), new AuthorDTO(alex));
+
+	    post1.setComments(Arrays.asList(comment1,comment3));
+	    post2.setComments(Arrays.asList(comment2));
+	    
 	    postRepo.saveAll(Arrays.asList(post1,post2));
 	    
-	    maria.getPosts().addAll(Arrays.asList(post1,post2));
+	    maria.getPosts().addAll(Arrays.asList(post1,post2));  
 	    
 	    userRepo.save(maria);
+	    
 	    
 	}
 
